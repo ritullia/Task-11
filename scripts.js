bcdb1e07d904bacd281c2b2093691c52217b74ec
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-19 17:56:08 +0200

    Develop (#1)
    * js fetch data commit
    
    * js fetch data & get post data commit

--- scripts.js
+++ scripts.js
@@ -1,19 +1,51 @@
 
 
+console.log("Veikia");
 
-BASE_URL = 'https://melon-potent-period.glitch.me';
+BASE_URL = 'https://melon-potent-period.glitch.me/skills';
 
+let data;
 
 fetch(BASE_URL)
     .then((response) => response.json())
     .then((result) => {
         console.log(result);
         data = result; //susigrazinam kintamaji
-        // drawCards(result); // paleidziame funkcija i fetch duomen pasiemimui
+        drawTable(result); // paleidziame funkcija i fetch duomen pasiemimui
     })
     .catch((error) => console.error(error));
 
+function drawTable(skillsArr) {
 
+    let mainSkillsTable = document.getElementById('skills');
+
+    skillsArr.forEach(data => {
+        let tBody = document.createElement("tbody");
+        tBody.classList.add('table-body')
+
+        let trEl = document.createElement('tr');
+        let thElm = document.createElement("th");
+        thElm.textContent = data.id
+        let thElmSec = document.createElement("th");
+        thElmSec.textContent = data.skill
+        let thElmBtn = document.createElement("th");
+        thElmBtn.textContent = data.action
+        let deletBtn = document.createElement("button")
+        deletBtn.textContent = "Delete";
+        deletBtn.classList.add("btn-delete")
+
+
+        thElmBtn.append(deletBtn)
+        trEl.append(thElm, thElmSec, thElmBtn)
+        tBody.append(trEl)
+        mainSkillsTable.append(tBody)
+
+
+    });
+
+
+
+}
 
 document.getElementById("btn").addEventListener('click', () => {
     window.location.href = "add.html"
