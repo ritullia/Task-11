945a7df361fa66560d8bf630e498c2314e3b7e75
ritullia <brigrita@gmail.com>
2023-02-19 16:03:54 +0200

    js fetch data commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
+console.log("Veikia");
 
+BASE_URL = 'https://melon-potent-period.glitch.me/skills';
 
-
-BASE_URL = 'https://melon-potent-period.glitch.me';
-
+let data;
 
 fetch(BASE_URL)
     .then((response) => response.json())
@@ -13,7 +13,17 @@ fetch(BASE_URL)
     })
     .catch((error) => console.error(error));
 
+function drawTable(skillsArr) {
 
+    let mainSkillsTable = document.getElementById('output');
+
+    skillsArr.forEach(data => {
+
+    });
+
+
+
+}
 
 document.getElementById("btn").addEventListener('click', () => {
     window.location.href = "add.html"
