9babf4fe45abfb5af42ebaed3934c2dce97ce086
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-19 17:57:12 +0200

    Dev (#2)
    * index html & style commit
    
    * web pages & buttons commit
    
    * Develop (#1)
    
    * js fetch data commit
    
    * js fetch data & get post data commit

---
+++ add.js
@@ -0,0 +1,33 @@
+console.log("Veikia")
+
+BASE_URL = 'https://melon-potent-period.glitch.me/skills';
+
+async function sendData() {
+
+    const skill = document.getElementById("skill-input").value;
+
+    const dataToSend = {
+        skill: skill,
+    };
+
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend),
+    });
+
+    if (response.ok === true) {
+        alert('Skill successfully added');
+        window.location.href = "./index.html"
+    } else {
+        alert('ERROR')
+    }
+}
+
+
+document.getElementById("btn-add").addEventListener('click', sendData)
+document.getElementById("btn-view").addEventListener('click', () => {
+    window.location.href = "./index.html"
+})
